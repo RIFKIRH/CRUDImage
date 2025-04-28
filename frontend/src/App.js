d15888715d32a55ae9246b3c43e4c705450e685e
RIFKIRH <rifkirahmatunh@gmail.com>
2025-04-28 11:00:25 +0700

    add page notfound

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import ProductList from './components/ProductList';
 import AddProduct from './components/AddProduct';
 import EditProduct from './components/EditProduct';
+import NotFoundPage from './components/404-page'; // Import the NotFoundPage component
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<ProductList />} />
         <Route path="add" element={<AddProduct />} />
         <Route path="edit/:id" element={<EditProduct />} />
+        <Route path="*" element={<NotFoundPage />} /> {/* Catch-all route for 404 */}
         {/* Add more routes here as needed */}
       </Routes>
     </BrowserRouter>
